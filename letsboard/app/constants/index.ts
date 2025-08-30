3478683cdfd3cb189baaec1bb62dd6f1f96ae9f5
Vishnu Kalanad <dxbvishnu@gmail.com>
2025-08-31 00:46:06 +0400

    desktop sidebar added

--- letsboard/app/constants/index.ts
+++ letsboard/app/constants/index.ts
@@ -11,7 +11,7 @@ export const sidebarItems = [
     id: 3,
     icon: "/assets/icons/users.svg",
     label: "All Users",
-    href: "/all-users",
+    href: "/users",
   },
   {
     id: 4,
--- letsboard/app/routes/admin/admin-layout.tsx
+++ letsboard/app/routes/admin/admin-layout.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Outlet} from "react-router";
+import {SidebarComponent} from "@syncfusion/ej2-react-navigations";
+import {MobileSidebar, NavItems} from "../../../components";
 
 function AdminLayout() {
     return (
         <div className={"admin-layout"}>
-            AdminLayout
-            <aside className="w-full max-w-[270px] hidden lg:block">
-                sidebar
+            <MobileSidebar />
+            <aside className="w-full h-full max-w-[270px] hidden lg:block">
+                <SidebarComponent width={270} height={"100%"} enableGestures={false}>
+                    <NavItems />
+                </SidebarComponent>
             </aside>
             <aside>
                 <Outlet />
